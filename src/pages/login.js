efccbe80b76ec0b76204c1c849196501b3f1c89c
zackster <ftchong235@yahoo.com>
2020-09-18 13:50:18 +0800

    #17 Signup and Auth State

--- src/pages/login.js
+++ src/pages/login.js
@@ -12,32 +12,9 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
-const styles = {
-  form: {
-    textAlign: "center",
-  },
-  image: {
-    margin: "20px auto 20px auto",
-  },
-  pageTitle: {
-    margin: "10px auto 10px",
-  },
-  textField: {
-    margin: "10px auto 10px",
-  },
-  button: {
-    marginTop: 20,
-    position: "relative",
-  },
-  customError: {
-    color: "red",
-    fontSize: "0.8rem",
-    marginTop: 10,
-  },
-  progress: {
-    position: "absolute",
-  },
-};
+const styles = (theme) => ({
+  ...theme,
+});
 
 class login extends Component {
   constructor() {
@@ -63,6 +40,7 @@ class login extends Component {
       .post("/login", userData)
       .then((res) => {
         console.log(res.data);
+        localStorage.setItem("FBIdToken", `Bearer ${res.data.token}`);
         this.setState({
           loading: false,
         });
@@ -109,8 +87,8 @@ class login extends Component {
             <TextField
               id="password"
               name="password"
-              type="Password"
-              label="password"
+              type="password"
+              label="Password"
               className={classes.textField}
               helperText={errors.password}
               error={errors.password ? true : false}
